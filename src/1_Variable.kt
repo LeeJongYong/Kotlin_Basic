c66edfac65484d5fce8f19cd1c9336e8b89be896
jongyong <jongyong924@gmail.com>
2024-05-16 20:22:12 +0900

    Kotlin 공부 1일차 - 변수 타입 - 함수 선언

---
+++ src/1_Variable.kt
@@ -0,0 +1,24 @@
+fun main() {
+    //  Kotlin 변수 선언
+    val a : Int = 1
+    val b = 2
+
+    val c : Int
+    c = 3
+
+    // val(value)       - 불변변수
+    // var(variable)    - 가변변수
+
+    val e : String = "Hello"
+    //e = "World"       - 불변변수이므로 값 변경X
+
+    var f = 123
+    //f = "World"       - 가변변수여도 데이터 타입이 다르면 값 변경X
+
+    println("Top level : " + z)
+
+}
+
+
+// 탑 레벨 변수  - 클래스 밖에서도 선언가능
+var z = 24
---
+++ src/2_Function.kt
@@ -0,0 +1,43 @@
+// Java 스타일
+fun sum(a:Int, b:Int) : Int {
+    return a+b
+}
+
+// 표현식 스타일
+fun sum2(a:Int, b:Int) : Int = a+b
+
+// 표현식 스타일  - 리턴타입X
+fun sum3(a:Int, b:Int) = a+b
+
+// return을 사용할 경우 리턴타입을 선언해줘야 한다.
+fun sum4(a:Int, b:Int) {
+//    return a+b
+}
+
+// 리턴타입이 없는 경우 Unit 타입을 반환한다.
+// Java의 void와 유사
+fun printSum(a:Int, b:Int) {
+    println("$a + $b = ${a+b}")
+}
+
+// 디폴트 param    - param이 없을경우 디폴트 파람을 사용한다.
+fun defaultParam(msg : String = "Kotlin을 시작합니다.") {
+    println(msg)
+}
+
+/*fun main() {
+    defaultParam()
+    defaultParam("예스예스 Kotlin")
+}*/
+
+fun log(level: String = "Info" ,message: String) {
+    println("[$level]$message")
+}
+
+fun main() {
+    log(message = "인포 로그")
+    log(level = "DEBUG", message = "디버그 로그")
+    log(level = "WARN", "워닝 로그")
+    log("ERROR", "에러 로그")
+
+}
